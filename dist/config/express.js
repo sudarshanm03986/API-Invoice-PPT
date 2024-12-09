81f48a027ac97f78794a79a1508fab5873231602
Sudarshan Malla <sudarshanm03986@gmail.com>
2024-12-10 09:19:55 +1300

    chnages

--- dist/config/express.js
+++ dist/config/express.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_middleware_1 = __importDefault(require("../app/middleware/cors.middleware"));
-const logger_1 = __importDefault(require("./logger"));
+// import Logger from "./logger"
 // import {rootUrl} from "../app/routes/base.routes";
 const rootUrl = "/api/v1";
 exports.default = () => {
@@ -17,12 +17,12 @@ exports.default = () => {
     app.use(body_parser_1.default.raw({ type: 'text/plain' }));
     app.use(body_parser_1.default.raw({ type: ['image/*'], limit: '5mb' }));
     // Debug
-    app.use((req, res, next) => {
-        if (req.path !== '/') {
-            logger_1.default.http(`##### ${req.method} ${req.path} #####`);
-        }
-        next();
-    });
+    // app.use((req, res, next) => {
+    //     if(req.path !== '/'){
+    //         // Logger.http(`##### ${req.method} ${req.path} #####`);
+    //     }
+    //     next();
+    // });
     app.get('/heartbeat', (req, res) => {
         res.send({ 'message': 'I\'m alive!' });
     });
--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,25 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("./config/express"));
 // Remove the MySQL connection import if you don't want to use it
 // import { connect } from './config/db';
-const logger_1 = __importDefault(require("./config/logger"));
+// import Logger from './config/logger'
+const dotenv_1 = __importDefault(require("dotenv"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 4941;
 // Remove the MySQL connection logic
 // Since you're not connecting to a database, you don't need that try-catch block
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            // If you're not using MySQL, simply skip this block
-            // await connect(); // Remove this line
-            app.listen(port, () => {
-                logger_1.default.info('Listening on port: http://localhost:' + port);
-            });
-        }
-        catch (err) {
-            logger_1.default.error('Unable to start the server.');
-            process.exit(1);
-        }
-    });
-}
-main().catch(err => logger_1.default.error(err));
+dotenv_1.default.config();
+app.listen(port, () => {
+    // Logger.info('Listening on port: ' + port)
+});
+module.exports = app;
+// async function main() {
+//     try {
+//         // If you're not using MySQL, simply skip this block
+//         // await connect(); // Remove this line
+//         app.listen(port, () => {
+//             Logger.info('Listening on port: http://localhost:' + port)
+//         });
+//     } catch (err) {
+//         Logger.error('Unable to start the server.')
+//         process.exit(1);
+//     }
+// }
+// main().catch(err => Logger.error(err));
 //# sourceMappingURL=server.js.map
